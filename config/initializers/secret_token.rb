66a78dbf66c08f429b043f09cb33f44b83138df9
Martin Kikegawa <mkikegawa@gmail.com>
2015-03-05 20:53:01 -0500

    Initial Chariot Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Chariot::Application.config.secret_token = '04734cc3253e8333151e26e04349243c54bac2d8d0ab0e0fff69ac3441589400a6126cd09798527b8d76908aa55669f50c910d56e22759e70f68d1df0b47814e'
