760725d38ef1057b53fdaae6e8be79e2184220e0
Martin Kikegawa <mkikegawa@gmail.com>
2015-03-05 21:04:17 -0500

    added User Resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create! ([
+  {name: 'Johnny Walker'},
+  {name: 'Gina Smith'},
+  {name: 'Bob Gill'},
+  {name: 'Sharon Young'}
+  ])
